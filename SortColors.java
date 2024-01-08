1f188f15138abd2193fae97467f1fcc85b96b11b
Zarin  Hadika <119046345+zarinhadika@users.noreply.github.com>
2024-01-08 10:36:13 +0600

    Add files via upload

---
+++ SortColors.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sortcolors;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author New Dynamic
+ */
+class Solution {
+    public void sortColors(int[] nums) {
+         Arrays.sort(nums);
+    }
+}
+public class SortColors {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
