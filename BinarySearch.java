0c02a1320cf4c7091690e0f87db135df0208170f
Zarin  Hadika <119046345+zarinhadika@users.noreply.github.com>
2024-01-16 10:46:46 +0600

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package binarysearch;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author New Dynamic
+ */
+
+public class BinarySearch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] intis={1,2,3,4,5,6,7};
+        System.out.println(Arrays.binarySearch(intis,9));
+    }
+    
+}
