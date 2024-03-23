e754b2c0b7780aff3bb21e7cddd2e733b8ac4437
Zarin  Hadika <119046345+zarinhadika@users.noreply.github.com>
2024-03-23 21:31:44 +0600

    Add files via upload

---
+++ FindPeakElement2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package findpeakelement2;
+
+/**
+ *
+ * @author New Dynamic
+ */
+class Solution {
+    public int findPeakElement(int[] nums) {
+    int position =0,current=nums[0];
+    for(int i=1;i<nums.length;i++)
+    {
+        if(nums[i]>current)
+        {
+            position =i;
+        } current =nums[i];
+    }
+      return position;
+    }
+}
+public class FindPeakElement2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
